76023ab25b8e5d49b94e8b3a8289cf47b15f209d
wangtao0101 <yuecjn@gmail.com>
2017-07-03 22:37:13 +0800

    design the way to inject model into mapToState and mapToProps

--- src/index.js
+++ src/index.js
@@ -33,6 +33,11 @@ const injectReducer = (key, red) => {
 // 1. 提供一个辅助dispatch的函数 dispatch(help(app, app.effects.add, {}))
 // 2. 提供一个mapDispatchToProps 包装函数
 // 以上两种方法考虑都提供
+// 最佳思路 提供一个Provider， 提供一个connect 包装 redux-connect 的 Provider 和 connect
+// Provider 比较简单，提供一个app的context即可
+// connect 包装 redux-connect的connect，首先拿到mapToProp 和mapToState， 包装处理后送给 redux-connect的connect
+// 包装组件参考 react-router的withRouter组件
+// https://github.com/ReactTraining/react-router/blob/v3.0.5/modules/withRouter.js
 const app = {
     namespace: '_',
     effects: {
