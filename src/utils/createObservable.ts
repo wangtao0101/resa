8bb911c52f69bb8bb4eb7c204277620cf8f0c923
wangtao0101 <yuecjn@gmail.com>
2018-10-11 15:44:09 +0800

    use defineProperty

--- src/utils/createObservable.ts
+++ src/utils/createObservable.ts
@@ -1,27 +1,20 @@
-// const createObservableProperty = function createObservableProperty(target, property) {
-//     Object.defineProperty(target, property, {
-//         get: function get() {
-//             return target[property];
-//         },
-//     });
-// };
-
-// const createObservable = function createObservable(target) {
-//     for(let i in target) {
-//         if(target.hasOwnProperty(i)) {
-//             createObservableProperty(target, i);
-//         }
-//     }
-// };
+const createObservableProperty = function createObservableProperty(target, property, value, depandenceMap) {
+    Object.defineProperty(target, property, {
+        get: function get() {
+            depandenceMap[property] = true;
+            return value;
+        },
+    });
+};
 
 const createObservable = function createObservable(target, depandenceMap) {
-    const handler = {
-        get(target, key, proxy) {
-            depandenceMap[key] = true;
-            return Reflect.get(target, key, proxy);
-        },
-    };
-    return new Proxy(target, handler);
-}
+    const newTarget = {}
+    for(let key in target) {
+        if(target.hasOwnProperty(key)) {
+            createObservableProperty(newTarget, key, target[key], depandenceMap);
+        }
+    }
+    return newTarget;
+};
 
 export default createObservable;
